c64723965e8f59bf7c47d2b3f86591a6cffa19a7
jberg <josep.bergay@gmail.com>
2021-12-18 19:35:40 +0100

    feat: add day10 part2

--- src/day10/index.ts
+++ src/day10/index.ts
@@ -18,7 +18,7 @@ const matching: Record<ChunkStart, ChunkEnd> = {
   "{": "}",
 };
 
-const chunkEndScore: Record<ChunkEnd, number> = {
+const syntaxCheckerScore: Record<ChunkEnd, number> = {
   ")": 3,
   ">": 25137,
   "]": 57,
@@ -55,13 +55,53 @@ const executePart1 = (input: ParsedInput): string => {
     }
   }
 
-  const score = illegalChars.reduce((p, c) => p + chunkEndScore[c], 0);
+  const score = illegalChars.reduce((p, c) => p + syntaxCheckerScore[c], 0);
 
   return `${score}`;
 };
 
+const autoCompleteScore: Record<ChunkEnd, number> = {
+  ")": 1,
+  "]": 2,
+  "}": 3,
+  ">": 4,
+};
+
+const findIncompleteStarts = (line: string) => {
+  const chunkStarts: ChunkStart[] = [];
+  for (const char of line) {
+    if (isChunkStart(char)) {
+      chunkStarts.push(char);
+    } else if (isChunkEnd(char)) {
+      chunkStarts.pop();
+    }
+  }
+  return chunkStarts;
+};
+
+const computeLineScore = (chunkStarts: ChunkStart[]) =>
+  chunkStarts
+    .reverse()
+    .reduce((p, s) => p * 5 + autoCompleteScore[matching[s]], 0);
+
 const executePart2 = (input: ParsedInput): string => {
-  return "";
+  const incompleteStarts: ChunkStart[][] = [];
+  for (const line of input) {
+    if (!findIllegalChar(line)) {
+      // Line is incomplete
+      const incomplete = findIncompleteStarts(line);
+      if (incomplete.length) {
+        incompleteStarts.push(incomplete);
+      }
+    }
+  }
+
+  const lineScores = incompleteStarts
+    .map(computeLineScore)
+    .sort((a, b) => a - b);
+  const middleScore = lineScores[Math.round((lineScores.length - 1) / 2)];
+
+  return `${middleScore}`;
 };
 
 const day10: AOCDay = async () => {
