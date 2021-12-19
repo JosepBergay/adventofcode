24df146e3581589f6eaeb9d80919ba160549c0db
jberg <josep.bergay@gmail.com>
2021-12-19 01:12:51 +0100

    feat: add logGrid and copyGrid utils

--- src/gridUtils.ts
+++ src/gridUtils.ts
@@ -9,3 +9,16 @@ export const isLeftEdge = ([x, y]: Point) => x === 0;
 
 export const isRightEdge = ([x, y]: Point, mapWidth: number) =>
   x === mapWidth - 1;
+
+export const logGrid = <T>(
+  grid: T[][],
+  prefix: string,
+  valueMapper?: (v: T) => any
+) =>
+  console.log(
+    prefix,
+    grid.map((row) => (valueMapper ? row.map(valueMapper).join() : row.join()))
+  );
+
+export const copyGrid = <T>(grid: T[][], mapper?: (v: T) => T) =>
+  grid.map((row) => (mapper ? row.map(mapper) : row.slice()));
