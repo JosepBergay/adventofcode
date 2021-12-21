5bfdd77d05ec2b08c45548125a7e52e5c14e702d
jberg <josep.bergay@gmail.com>
2021-12-21 01:15:42 +0100

    refactor: extract buildGrid

--- src/gridUtils.ts
+++ src/gridUtils.ts
@@ -22,3 +22,22 @@ export const logGrid = <T>(
 
 export const copyGrid = <T>(grid: T[][], mapper?: (v: T) => T) =>
   grid.map((row) => (mapper ? row.map(mapper) : row.slice()));
+
+export const buildGrid = <T>(points: Point[], value: T, emptyValue: T) => {
+  const max_x = points.reduce((max, [x, _]) => (x > max ? x : max), 0);
+  const max_y = points.reduce((max, [_, y]) => (y > max ? y : max), 0);
+
+  const grid: T[][] = [];
+  for (let y = 0; y < max_y + 1; y++) {
+    const row: T[] = [];
+    for (let x = 0; x < max_x + 1; x++) {
+      row.push(emptyValue);
+    }
+    grid.push(row);
+  }
+
+  for (const [x, y] of points) {
+    grid[y][x] = value;
+  }
+  return grid;
+};
