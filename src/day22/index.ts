799168dd87aad77155dbe01f61b473643b8bb2f8
jberg <josep.bergay@gmail.com>
2021-12-31 19:24:58 +0100

    feat: add day22 part2

--- src/day22/index.ts
+++ src/day22/index.ts
@@ -86,28 +86,77 @@ const executePart1 = (input: ParsedInput) => {
   return grid.count((isOn) => isOn);
 };
 
+type Cube = {
+  x1: number;
+  x2: number;
+  y1: number;
+  y2: number;
+  z1: number;
+  z2: number;
+};
+
+type Cuboid = {
+  cube: Cube;
+  isOn: boolean;
+  holes: Cuboid[];
+};
+
+type Proc = Omit<Cuboid, "holes">;
+
+const intersect = (a: Cube, b: Cube) => {
+  const x1 = Math.max(a.x1, b.x1);
+  const x2 = Math.min(a.x2, b.x2);
+  const y1 = Math.max(a.y1, b.y1);
+  const y2 = Math.min(a.y2, b.y2);
+  const z1 = Math.max(a.z1, b.z1);
+  const z2 = Math.min(a.z2, b.z2);
+
+  if (x2 >= x1 && y2 >= y1 && z2 >= z1) {
+    return { x1, x2, y1, y2, z1, z2 } as Cube;
+  }
+  return false;
+};
+
+const getHole = (a: Cuboid, b: Cube): Cuboid | null => {
+  const intersection = intersect(a.cube, b);
+  if (!intersection) {
+    return null;
+  }
+  // If intersecting, create a hole of the opposite state to not count it twice.
+  return {
+    cube: intersection,
+    isOn: !a.isOn,
+    holes: a.holes.map((h) => getHole(h, b)).filter((h) => h) as Cuboid[],
+  };
+};
+
+const countCubes = ({ cube, holes }: Cuboid) => {
+  let total =
+    (cube.x2 - cube.x1 + 1) * (cube.y2 - cube.y1 + 1) * (cube.z2 - cube.z1 + 1);
+  total -= holes.reduce((p, c) => p + countCubes(c), 0);
+  return total;
+};
+
 const executePart2 = (input: ParsedInput) => {
-//   const grid = create3DGrid<boolean>();
+  const procs = input.map(
+    ([a, [x1, x2], [y1, y2], [z1, z2]]) =>
+      ({ cube: { x1, x2, y1, y2, z1, z2 }, isOn: a === "on" } as Proc)
+  );
 
-//   const set = new Set();
+  const parsed: Cuboid[] = [];
+  for (const proc of procs) {
+    for (const p of parsed) {
+      const hole = getHole(p, proc.cube);
+      if (hole) {
+        p.holes.push(hole);
+      }
+    }
 
-//   for (const [a, rangeX, rangeY, rangeZ] of input) {
-//     for (let x = rangeX[0]; x <= rangeX[1]; x++) {
-//       for (let y = rangeY[0]; y <= rangeY[1]; y++) {
-//         for (let z = rangeZ[0]; z <= rangeZ[1]; z++) {
-//           if (a === "on") {
-//             set.add(`${x},${y},${z}`);
-//           } else {
-//             set.delete(`${x},${y},${z}`);
-//           }
-//         //   console.log(set.size);
-//           //   grid.set(x, y, z, a === "on");
-//         }
-//       }
-//     }
-//   }
-//   return set.size;
-//   return grid.count((isOn) => isOn);
+    if (proc.isOn) {
+      parsed.push({ ...proc, holes: [] });
+    }
+  }
+  return parsed.reduce((p, c) => p + countCubes(c), 0);
 };
 
 const day22: AOCDay = async () => {
