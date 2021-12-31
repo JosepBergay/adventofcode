7d7c104cae1da6d6b140dae3abf2b0eeb742ed41
jberg <josep.bergay@gmail.com>
2021-12-31 13:37:34 +0100

    feat: add day21 part2

--- src/day21/index.ts
+++ src/day21/index.ts
@@ -27,12 +27,10 @@ const playDiracDice = (start1: number, start2: number) => {
 
   const movePawn = (player: 1 | 2, rolled: number) => {
     if (player === 1) {
-      const aux = (position1 + rolled) % 10;
-      position1 = aux ? aux : 10;
+      position1 = (position1 + rolled) % 10 || 10;
       return position1;
     } else {
-      const aux = (position2 + rolled) % 10;
-      position2 = aux ? aux : 10;
+      position2 = (position2 + rolled) % 10 || 10;
       return position2;
     }
   };
@@ -57,6 +55,68 @@ const playDiracDice = (start1: number, start2: number) => {
   return { score1, score2, rollCount };
 };
 
+const playQuantumDice = (start1: number, start2: number) => {
+  const diceMap: Record<number, number> = {};
+  // Compute probability
+  for (let i = 1; i <= 3; i++) {
+    for (let j = 1; j <= 3; j++) {
+      for (let k = 1; k <= 3; k++) {
+        const sum = i + j + k;
+        diceMap[sum] = (diceMap[sum] || 0) + 1;
+      }
+    }
+  }
+
+  let wins1Count = 0;
+  let wins2Count = 0;
+
+  const play = (
+    player: 1 | 2,
+    pos1: number,
+    pos2: number,
+    score1: number,
+    score2: number,
+    accUniverses: number
+  ) => {
+    for (let roll = 3; roll <= 9; roll++) {
+      const universes = accUniverses * diceMap[roll];
+
+      if (player === 1) {
+        // Save state for next loop
+        let currentP = pos1;
+        let currentS = score1;
+        // Apply
+        pos1 = (pos1 + roll) % 10 || 10;
+        score1 += pos1;
+
+        if (score1 >= 21) wins1Count += universes;
+        else play(2, pos1, pos2, score1, score2, universes);
+
+        // Reset state for next loop
+        pos1 = currentP;
+        score1 = currentS;
+      } else {
+        // Same as before but for player2
+        let currentP = pos2;
+        let currentS = score2;
+
+        pos2 = (pos2 + roll) % 10 || 10;
+        score2 += pos2;
+
+        if (score2 >= 21) wins2Count += universes;
+        else play(1, pos1, pos2, score1, score2, universes);
+
+        pos2 = currentP;
+        score2 = currentS;
+      }
+    }
+  };
+
+  play(1, start1, start2, 0, 0, 1);
+
+  return { wins1Count, wins2Count };
+};
+
 const executePart1 = (input: ParsedInput) => {
   const { score1, score2, rollCount } = playDiracDice(input[0], input[1]);
 
@@ -66,7 +126,9 @@ const executePart1 = (input: ParsedInput) => {
 };
 
 const executePart2 = (input: ParsedInput) => {
-  return "";
+  const { wins1Count, wins2Count } = playQuantumDice(input[0], input[1]);
+
+  return wins1Count > wins2Count ? wins1Count : wins2Count;
 };
 
 const day21: AOCDay = async () => {
