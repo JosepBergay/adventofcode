d6302775d153846715879519d683247c53aa5c9f
jberg <josep.bergay@gmail.com>
2022-12-02 23:24:15 +0100

    feat: add day1 part1

---
+++ src/2022/day1.go
@@ -0,0 +1,88 @@
+package aoc2022
+
+import (
+	"strconv"
+	"strings"
+)
+
+const dayNum = 1
+
+func init() {
+	Days[dayNum] = &day{}
+}
+
+type day struct{}
+
+func (d *day) Day() int {
+	return dayNum
+}
+
+func (d *day) Parse(input string) ([][]int, error) {
+	strGroups := strings.Split(input, "\n\n")
+
+	intGroups := make([][]int, len(strGroups))
+
+	for i, v := range strGroups {
+		strCalories := strings.Split(v, "\n")
+
+		intCalories := make([]int, len(strCalories))
+		for j, v := range strCalories {
+			foodCalories, err := strconv.Atoi(v)
+
+			if err != nil {
+				continue
+			}
+
+			intCalories[j] = foodCalories
+		}
+
+		intGroups[i] = intCalories
+	}
+	return intGroups, nil
+}
+
+func (d *day) Part1(groups [][]int) (string, error) {
+	most := 0
+
+	for _, calories := range groups {
+		curr := 0
+
+		for _, v := range calories {
+			curr += v
+		}
+
+		if most < curr {
+			most = curr
+		}
+	}
+
+	return strconv.Itoa(most), nil
+}
+
+func (d *day) Part2(parsed [][]int) (string, error) {
+	return "TODO", nil
+}
+
+func (d *day) Exec(input string) (*DayResult, error) {
+	parsed, err := d.Parse(input)
+
+	if err != nil {
+		return nil, err
+	}
+
+	part1, err := d.Part1(parsed)
+
+	if err != nil {
+		return nil, err
+	}
+
+	part2, err := d.Part2(parsed)
+
+	if err != nil {
+		return nil, err
+	}
+
+	result := &DayResult{part1, part2}
+
+	return result, nil
+}
---
+++ src/2022/day1_test.go
@@ -0,0 +1,67 @@
+package aoc2022
+
+import "testing"
+
+const input = `1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+`
+
+const expectedP1 = "24000"
+
+func TestDay1Part1(t *testing.T) {
+	day := &day{}
+
+	parsed, err := day.Parse(input)
+
+	if err != nil {
+		t.Errorf("[Parse]: %v", err.Error())
+		return
+	}
+
+	res, err := day.Part1(parsed)
+
+	if err != nil {
+		t.Errorf("[Part1]: %v", err.Error())
+		return
+	}
+
+	if res != expectedP1 {
+		t.Errorf("Expected: %v \nBut got: %v", expectedP1, res)
+	}
+}
+
+const expectedP2 = "TODO"
+
+func TestDay1Part2(t *testing.T) {
+	day := &day{}
+
+	parsed, err := day.Parse(input)
+
+	if err != nil {
+		t.Errorf("[Parse]: %v", err.Error())
+		return
+	}
+
+	res, err := day.Part2(parsed)
+
+	if err != nil {
+		t.Errorf("[Part2]: %v", err.Error())
+		return
+	}
+
+	if res != expectedP2 {
+		t.Errorf("Expected: %v \nBut got: %v", expectedP2, res)
+	}
+}
