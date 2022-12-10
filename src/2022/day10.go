57859decbb09aa202a24df1140176bd25492ec51
jberg <josep.bergay@gmail.com>
2022-12-10 17:45:06 +0100

    feat: add day10 part2

--- src/2022/day10.go
+++ src/2022/day10.go
@@ -30,12 +30,10 @@ func isInterestingCycle(cycle int) bool {
 	return false
 }
 
-func (d *day10) Part1(input string) (string, error) {
+func executeSignals(input string, duringEachCycle func(cycle, registerX int)) error {
 	reader := strings.NewReader(input)
 	scanner := bufio.NewScanner(reader)
 	scanner.Split(bufio.ScanLines)
-
-	sum := 0
 
 	registerX := 1
 	cycleCount := 0
@@ -45,9 +43,7 @@ func (d *day10) Part1(input string) (string, error) {
 	for {
 		cycleCount++
 
-		if isInterestingCycle(cycleCount) {
-			sum += cycleCount * registerX // Compute signal strength *DURING* cycle
-		}
+		duringEachCycle(cycleCount, registerX)
 
 		// We fake a cycle when we have a pending value from previous cycle.
 		if pending != int(math.Inf(1)) {
@@ -71,13 +67,31 @@ func (d *day10) Part1(input string) (string, error) {
 		strs := strings.Split(instr, " ")
 		v, err := strconv.Atoi(strs[1])
 		if err != nil {
-			return "", err
+			return err
 		}
 
 		pending = v
 	}
 
 	if err := scanner.Err(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (d *day10) Part1(input string) (string, error) {
+	sum := 0
+
+	duringEachCycle := func(cycle, registerX int) {
+		if isInterestingCycle(cycle) {
+			sum += cycle * registerX
+		}
+	}
+
+	err := executeSignals(input, duringEachCycle)
+
+	if err != nil {
 		return "", err
 	}
 
@@ -85,7 +99,45 @@ func (d *day10) Part1(input string) (string, error) {
 }
 
 func (d *day10) Part2(input string) (string, error) {
-	return "TODO", nil
+	// Fancy (and efficient) way of building a string. Could also use += or fmt.Sprintf.
+	var crt strings.Builder
+
+	duringEachCycle := func(cycle, registerX int) {
+		if cycle > 40*6 {
+			// The way executeSignals is written makes duringEachCycle execute once more just
+			// before EOF, so we skip this pixel.
+			return
+		}
+
+		pixelPos := (cycle - 1) % 40
+		if pixelPos == 0 {
+			crt.WriteString("\n")
+		}
+
+		pixel := "."
+		// Sprite is 3px wide
+		if registerX-1 <= pixelPos && pixelPos <= registerX+1 {
+			pixel = "#"
+		}
+
+		crt.WriteString(pixel)
+	}
+
+	err := executeSignals(input, duringEachCycle)
+
+	if err != nil {
+		return "", err
+	}
+
+	/** Pixel perfect!
+	 * ###..####.####.####.#..#.###..####..##..
+	 * #..#.#.......#.#....#.#..#..#.#....#..#.
+	 * #..#.###....#..###..##...###..###..#..#.
+	 * ###..#.....#...#....#.#..#..#.#....####.
+	 * #.#..#....#....#....#.#..#..#.#....#..#.
+	 * #..#.#....####.####.#..#.###..#....#..#.
+	 */
+	return crt.String(), nil
 }
 
 func (d *day10) Exec(input string) (*DayResult, error) {
