818d4aea2a7dc4f498b4eaa65a89beef01a12f01
jberg <josep.bergay@gmail.com>
2022-12-18 21:15:16 +0100

    feat: add day15 part1

---
+++ src/2022/day15.go
@@ -0,0 +1,182 @@
+package aoc2022
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"math"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type day15 struct {
+	cave             map[Point]string
+	sensorsToBeacons map[Point]Point
+	row              int
+	minX             int
+	maxX             int
+	minY             int
+	maxY             int
+}
+
+func init() {
+	Days[15] = &day15{}
+}
+
+func (d *day15) addToMap(p Point, mark string) error {
+	if mark != "S" && mark != "B" && mark != "#" {
+		return fmt.Errorf("%v is not valid mark", mark)
+	}
+
+	_, exists := d.cave[p]
+
+	if exists {
+		return nil
+	}
+
+	if p.x > d.maxX {
+		d.maxX = p.x
+	} else if p.x < d.minX {
+		d.minX = p.x
+	}
+
+	if p.y > d.maxY {
+		d.maxY = p.y
+	} else if p.y < d.minY {
+		d.minY = p.y
+	}
+
+	d.cave[p] = mark
+
+	return nil
+}
+
+func (d *day15) Parse(input string) (string, error) {
+	scanner := bufio.NewScanner(strings.NewReader(input))
+
+	re := regexp.MustCompile(`[-]?\d+`)
+
+	d.cave = make(map[Point]string)
+	d.sensorsToBeacons = make(map[Point]Point)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		bytes := re.FindAll([]byte(line), -1)
+		if len(bytes) != 4 {
+			return "", errors.New("line has not 4 numbers")
+		}
+
+		coords := [4]int{}
+		for i, b := range bytes {
+			coord, err := strconv.Atoi(string(b))
+			if err != nil {
+				return "", err
+			}
+			coords[i] = coord
+		}
+
+		sensor := Point{coords[0], coords[1]}
+		beacon := Point{coords[2], coords[3]}
+		d.addToMap(sensor, "S")
+		d.addToMap(beacon, "B")
+		d.sensorsToBeacons[sensor] = beacon
+	}
+
+	return "", nil
+}
+
+// TRBL includes Top, Right, Bottom, Left directions in this order.
+var TRBL = [4]Point{
+	{0, 1},
+	{1, 0},
+	{0, -1},
+	{-1, 0},
+}
+
+// Stack Overflow T.T
+func (d *day15) walkSpace(curr Point, visited map[Point]bool, outOfBounds func(curr Point) bool) {
+	if outOfBounds(curr) {
+		return
+	}
+
+	if visited[curr] {
+		return
+	}
+
+	d.addToMap(curr, "#")
+	visited[curr] = true
+
+	// Recurse
+	for _, dir := range TRBL {
+		next := Point{curr.x + dir.x, curr.y + dir.y}
+		d.walkSpace(next, visited, outOfBounds)
+	}
+}
+
+func getManhattanDistance(p1, p2 Point) int {
+	x := p1.x - p2.x
+	y := p1.y - p2.y
+	return int(math.Abs(float64(x)) + math.Abs(float64(y)))
+}
+
+func (d *day15) Part1(input string) (string, error) {
+	// Update map min/max X
+	for sensor, beacon := range d.sensorsToBeacons {
+		manhattan := getManhattanDistance(sensor, beacon)
+
+		if sensor.x-manhattan < d.minX {
+			d.minX = sensor.x - manhattan
+		}
+
+		if sensor.x+manhattan > d.maxX {
+			d.maxX = sensor.x + manhattan
+		}
+	}
+
+	out := 0
+	for x := d.minX; x <= d.maxX; x++ {
+		p := Point{x, d.row}
+
+		for sensor, beacon := range d.sensorsToBeacons {
+			manhattan := getManhattanDistance(sensor, beacon)
+
+			if _, ok := d.cave[p]; !ok && getManhattanDistance(sensor, p) <= manhattan {
+				out++
+				break
+			}
+		}
+	}
+
+	return fmt.Sprint(out), nil
+}
+
+func (d *day15) Part2(input string) (string, error) {
+	return "TODO", nil
+}
+
+func (d *day15) Exec(input string) (*DayResult, error) {
+	parsed, err := d.Parse(input)
+
+	if err != nil {
+		return nil, err
+	}
+
+	d.row = 2_000_000
+	part1, err := d.Part1(parsed)
+
+	if err != nil {
+		return nil, err
+	}
+
+	part2, err := d.Part2(parsed)
+
+	if err != nil {
+		return nil, err
+	}
+
+	result := &DayResult{part1, part2}
+
+	return result, nil
+}
---
+++ src/2022/day15_test.go
@@ -0,0 +1,68 @@
+package aoc2022
+
+import "testing"
+
+const inputD15 = `Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+Sensor at x=9, y=16: closest beacon is at x=10, y=16
+Sensor at x=13, y=2: closest beacon is at x=15, y=3
+Sensor at x=12, y=14: closest beacon is at x=10, y=16
+Sensor at x=10, y=20: closest beacon is at x=10, y=16
+Sensor at x=14, y=17: closest beacon is at x=10, y=16
+Sensor at x=8, y=7: closest beacon is at x=2, y=10
+Sensor at x=2, y=0: closest beacon is at x=2, y=10
+Sensor at x=0, y=11: closest beacon is at x=2, y=10
+Sensor at x=20, y=14: closest beacon is at x=25, y=17
+Sensor at x=17, y=20: closest beacon is at x=21, y=22
+Sensor at x=16, y=7: closest beacon is at x=15, y=3
+Sensor at x=14, y=3: closest beacon is at x=15, y=3
+Sensor at x=20, y=1: closest beacon is at x=15, y=3`
+
+const expectedD15P1 = "26"
+
+func TestDay15Part1(t *testing.T) {
+	day := &day15{}
+
+	day.row = 10
+
+	parsed, err := day.Parse(inputD15)
+
+	if err != nil {
+		t.Errorf("[Parse]: %v", err.Error())
+		return
+	}
+
+	res, err := day.Part1(parsed)
+
+	if err != nil {
+		t.Errorf("[Part1]: %v", err.Error())
+		return
+	}
+
+	if res != expectedD15P1 {
+		t.Errorf("Expected: %v \nBut got: %v", expectedD15P1, res)
+	}
+}
+
+const expectedD15P2 = ""
+
+func TestDay15Part2(t *testing.T) {
+	day := &day15{}
+
+	parsed, err := day.Parse(inputD15)
+
+	if err != nil {
+		t.Errorf("[Parse]: %v", err.Error())
+		return
+	}
+
+	res, err := day.Part2(parsed)
+
+	if err != nil {
+		t.Errorf("[Part2]: %v", err.Error())
+		return
+	}
+
+	if res != expectedD15P2 {
+		t.Errorf("Expected: %v \nBut got: %v", expectedD15P2, res)
+	}
+}
