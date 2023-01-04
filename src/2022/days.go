107db76339c0100af1ff2f611ef3f4e318219e72
jberg <josep.bergay@gmail.com>
2023-01-04 22:05:32 +0100

    feat: get input from file, if it doesn't exist fetch and write to file

--- src/2022/days.go
+++ src/2022/days.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 )
 
 type DayResult struct {
@@ -17,7 +18,38 @@ type Day interface {
 
 var Days = make(map[int]Day, 0)
 
+const year = 2022
+
 func FetchInput(day int, cookie string) (string, error) {
+	filePath := createInputFilePath(year, day)
+
+	_, err := os.Stat(filePath)
+	if err != nil {
+		body, err := fetchInput(day, cookie)
+		if err != nil {
+			return "", err
+		}
+
+		if err := writeToFile(filePath, body); err != nil {
+			return "", err
+		}
+
+		return string(body), nil
+	}
+
+	b, err := os.ReadFile(filePath)
+	if err != nil {
+		return "", err
+	}
+
+	return string(b), err
+}
+
+func createInputFilePath(year, day int) string {
+	return fmt.Sprintf("./src/%v/day%v.txt", year, day)
+}
+
+func fetchInput(day int, cookie string) ([]byte, error) {
 	req, err := http.NewRequest("GET", fmt.Sprintf("https://adventofcode.com/2022/day/%v/input", day), nil)
 
 	if err != nil {
@@ -30,17 +62,32 @@ func FetchInput(day int, cookie string) (string, error) {
 	client := &http.Client{}
 
 	resp, err := client.Do(req)
-
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
 
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
-	return string(body), nil
+	return body, nil
+}
+
+func writeToFile(fileName string, body []byte) error {
+	f, err := os.Create(fileName)
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+
+	_, err = f.Write(body)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
