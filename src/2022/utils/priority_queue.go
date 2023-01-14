e5aa78f792cfc09b2e06ad7cdd542c84c108965f
jberg <josep.bergay@gmail.com>
2023-01-14 18:30:28 +0100

    feat: add priority queue to utils

---
+++ src/2022/utils/priority_queue.go
@@ -0,0 +1,56 @@
+package utils
+
+import "container/heap"
+
+// Based on "container/heap" -> "heap_test" -> "example_pq_test"
+
+// An Item is something we manage in a priority queue.
+type Item[T any] struct {
+	value    *T  // The value of the item; arbitrary.
+	priority int // The priority of the item in the queue.
+	// The index is needed by update and is maintained by the heap.Interface methods.
+	index int // The index of the item in the heap.
+}
+
+// A PriorityQueue implements heap.Interface and holds Items.
+type PriorityQueue[T any] []*Item[T]
+
+func (pq PriorityQueue[T]) Len() int { return len(pq) }
+
+func (pq PriorityQueue[T]) Less(i, j int) bool {
+	// We want Pop to give us the highest, not lowest, priority so we use greater than here.
+	return pq[i].priority > pq[j].priority
+}
+
+func (pq PriorityQueue[T]) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
+func (pq *PriorityQueue[T]) Push(x any) {
+	n := len(*pq)
+	item := x.(*Item[T])
+	item.index = n
+	*pq = append(*pq, item)
+}
+
+func (pq *PriorityQueue[T]) Pop() any {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil  // avoid memory leak
+	item.index = -1 // for safety
+	*pq = old[0 : n-1]
+	return item
+}
+
+func (pq *PriorityQueue[T]) Insert(value *T, priority int) {
+	item := Item[T]{value, priority, 0}
+	heap.Push(pq, item)
+}
+
+func (pq *PriorityQueue[T]) Delete() *T {
+	item := heap.Pop(pq).(*Item[T])
+	return item.value
+}
