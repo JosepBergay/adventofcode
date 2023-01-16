ecd7972c23691643cfc77ad1cb89a3a1d9b5da2b
jberg <josep.bergay@gmail.com>
2023-01-16 21:06:27 +0100

    feat: add day25 part1

--- src/2022/day25.go
+++ src/2022/day25.go
@@ -1,20 +1,77 @@
 package aoc2022
 
+import (
+	"fmt"
+	"math"
+	"strings"
+)
+
 type day25 struct{}
 
 func init() {
 	Days[25] = &day25{}
 }
 
-func (d *day25) Parse(input string) (string, error) {
-	return "TODO", nil
+func (d *day25) Parse(input string) ([]string, error) {
+	split := strings.Split(input, "\n")
+
+	return split[:len(split)-1], nil
 }
 
-func (d *day25) Part1(input string) (string, error) {
-	return "TODO", nil
+var snafuToIntDigit = map[rune]int{
+	'2': 2,
+	'1': 1,
+	'0': 0,
+	'-': -1,
+	'=': -2,
 }
 
-func (d *day25) Part2(input string) (string, error) {
+var intToSnafuDigit = map[int]string{
+	2:  "2",
+	1:  "1",
+	0:  "0",
+	-1: "-",
+	-2: "=",
+}
+
+func fromSNAFU(snafu string) int {
+	out := 0
+
+	for i, r := range snafu {
+		d := snafuToIntDigit[r]
+
+		out += d * int(math.Pow(5, float64(len(snafu)-i-1)))
+	}
+
+	return out
+}
+
+func toSNAFU(x int) string {
+	snafu := ""
+	curr := x
+	for curr > 0 {
+		mod := ((curr + 2) % 5) - 2
+		curr = (curr + 2) / 5
+
+		snafu = intToSnafuDigit[mod] + snafu
+	}
+
+	return snafu
+}
+
+func (d *day25) Part1(input []string) (string, error) {
+	sum := 0
+
+	for _, s := range input {
+		sum += fromSNAFU(s)
+	}
+
+	snafu := toSNAFU(sum)
+
+	return fmt.Sprint(snafu), nil
+}
+
+func (d *day25) Part2(input []string) (string, error) {
 	return "TODO", nil
 }
 
