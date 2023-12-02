1ab594b7a398de31d7e056e5a4b7f2c06e946673
jberg <josep.bergay@gmail.com>
2023-12-02 14:14:10 +0100

    feat: add day2 part1

--- src/2023/days/allDays.kt
+++ src/2023/days/allDays.kt
@@ -3,6 +3,7 @@ package aoc2023.days
 fun getDay(day: Int): BaseDay? {
     return when (day) {
         1 -> Day1()
+        2 -> Day2()
         else -> null
     }
 }
---
+++ src/2023/days/day2.kt
@@ -0,0 +1,51 @@
+package aoc2023.days
+
+import kotlin.io.path.readLines
+
+typealias Cube = String
+
+data class GameSet(val cubes: Map<Cube, Int>)
+
+data class Game(val id: Int, val sets: Collection<GameSet>)
+
+class Day2 : BaseDay(2) {
+    var games: List<Game> = listOf()
+
+    override fun parse() {
+        games =
+                inputPath.readLines().filter { !it.isNullOrEmpty() }.mapIndexed { idx, it ->
+                    val sets =
+                            it.split(":")[1].split(";").map {
+                                val pairs =
+                                        it.split(",").map {
+                                            val cubesStr = it.trim().split(" ")
+
+                                            cubesStr[1] to cubesStr[0].toInt()
+                                        }
+
+                                GameSet(pairs.toMap())
+                            }
+
+                    Game(idx + 1, sets)
+                }
+    }
+
+    override fun part1(): Int {
+        val bag = mapOf("red" to 12, "green" to 13, "blue" to 14)
+
+        return games.sumOf {
+            val valid = it.sets.all { it.cubes.all { it.key in bag && it.value <= bag[it.key]!! } }
+
+            if (valid) it.id else 0
+        }
+    }
+
+    override fun part2(): String {
+
+        return "TODO"
+    }
+}
+
+// 4888
+
+// 2183
