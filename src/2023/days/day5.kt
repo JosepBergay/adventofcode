3576ca3caa50cfcb2046a8881b4ba0fb3971cf89
jberg <josep.bergay@gmail.com>
2023-12-06 01:20:00 +0100

    feat: add day5 part

--- src/2023/days/day5.kt
+++ src/2023/days/day5.kt
@@ -2,29 +2,30 @@ package aoc2023.days
 
 import kotlin.io.path.*
 
-data class SeedMappingState(
-        var mapped: MutableList<Long>,
-        var remaining: MutableList<Long>,
+data class SeedMappingState<T>(
+        var mapped: MutableList<T>,
+        var remaining: MutableList<T>,
         var stage: Int
 )
 
+val digitsRegex = """\d+""".toRegex()
+
 class Day5 : BaseDay(5) {
     var input = listOf<String>()
+    var initialSeeds = emptySequence<Long>()
 
     override fun parse() {
         input = inputPath.readLines()
         // input = testInputD5.reader().readLines()
+
+        initialSeeds = digitsRegex.findAll(input[0]).map { it.value.toLong() }
     }
 
     override fun part1(): Long {
-        val digitsRegex = """\d+""".toRegex()
+        val initialState = SeedMappingState(mutableListOf(), initialSeeds.toMutableList(), 0)
 
-        val seeds = digitsRegex.findAll(input[0]).map { it.value.toLong() }.toMutableList()
-
-        val initialState = SeedMappingState(mutableListOf(), seeds, 0)
-
-        val out =
-                input.drop(1).fold(initialState) { state, curr ->
+        return input.drop(1)
+                .fold(initialState) { state, curr ->
                     if (curr.isEmpty()) {
                         // Next stage
                         state.stage++
@@ -44,22 +45,83 @@ class Day5 : BaseDay(5) {
                     val len = range[2]
                     val offset = sourceStart - destinationStart
 
-                    val groups = state.remaining.groupBy { it in sourceStart..(sourceStart + len) }
+                    val (inRange, outOfRange) =
+                            state.remaining.partition { it in sourceStart..sourceStart + len }
 
-                    val inRange = groups.get(true) ?: mutableListOf()
-
-                    state.mapped.addAll(inRange.toMutableList().map { it - offset }.toMutableList())
-                    state.remaining.removeAll { it in inRange }
+                    state.mapped.addAll(inRange.map { it - offset })
+                    state.remaining = outOfRange.toMutableList()
 
                     state
                 }
-
-        return (out.remaining + out.mapped).min()
+                .let { (it.remaining + it.mapped).min() }
     }
 
-    override fun part2(): String {
+    override fun part2(): Long {
+        val seedRanges = initialSeeds.chunked(2).map { it[0]..(it[0] + it[1]) }.toMutableList()
 
-        return "TODO"
+        val initialState = SeedMappingState(mutableListOf(), seedRanges, 0)
+
+        return input.drop(1)
+                .fold(initialState) { state, curr ->
+                    if (curr.isEmpty()) {
+                        // Next stage
+                        state.stage++
+                        state.remaining += state.mapped
+                        state.mapped = mutableListOf()
+
+                        return@fold state
+                    } else if (!curr[0].isDigit()) {
+                        // source-to-destination map:
+                        return@fold state
+                    }
+
+                    val range = digitsRegex.findAll(curr).map { it.value.toLong() }.toList()
+
+                    val destinationStart = range[0]
+                    val sourceStart = range[1]
+                    val len = range[2]
+                    val offset = sourceStart - destinationStart
+                    val validRange = sourceStart..sourceStart + len
+
+                    val initial = mutableListOf<LongRange>() to mutableListOf<LongRange>()
+
+                    val (inRange, outOfRange) =
+                            state.remaining.fold(initial) { (inR, outR), curr ->
+                                when {
+                                    // Totally out of range
+                                    curr.last < validRange.first || validRange.last < curr.first ->
+                                            inR to outR.apply { add(curr) }
+                                    // Fully in range
+                                    validRange.first <= curr.first &&
+                                            curr.last <= validRange.last ->
+                                            inR.apply { add(curr) } to outR
+                                    // Partially in range
+                                    else -> {
+                                        if (curr.first < validRange.first &&
+                                                        validRange.last < curr.last
+                                        ) {
+                                            outR.add(curr.first ..< validRange.first)
+                                            inR.add(validRange)
+                                            outR.add(validRange.last + 1..curr.last)
+                                        } else if (curr.first < validRange.first) {
+                                            outR.add(curr.first ..< validRange.first)
+                                            inR.add(validRange.first..curr.last)
+                                        } else {
+                                            inR.add(curr.first..validRange.last)
+                                            outR.add(validRange.last + 1..curr.last)
+                                        }
+
+                                        inR to outR
+                                    }
+                                }
+                            }
+
+                    state.mapped.addAll(inRange.map { (it.first - offset)..(it.last - offset) })
+                    state.remaining = outOfRange.toMutableList()
+
+                    state
+                }
+                .let { (it.remaining + it.mapped).map { it.first }.min() }
     }
 }
 
