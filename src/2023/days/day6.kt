5bcceab7ad6319c64250206cddb6bc2138b8a0ff
jberg <josep.bergay@gmail.com>
2023-12-06 20:10:02 +0100

    feat: add day6 part2

--- src/2023/days/day6.kt
+++ src/2023/days/day6.kt
@@ -5,31 +5,32 @@ import kotlin.io.path.readLines
 data class RaceInfo(var time: Int, var distance: Int)
 
 class Day6 : BaseDay(6) {
-    val input = mutableListOf<RaceInfo>()
+    val times = mutableListOf<String>()
+    val distances = mutableListOf<String>()
 
     override fun parse() {
         val lines = inputPath.readLines()
 
-        input.addAll(digitsRegex.findAll(lines[0]).map { RaceInfo(it.value.toInt(), 0) })
+        times.addAll(digitsRegex.findAll(lines[0]).map { it.value })
+        distances.addAll(digitsRegex.findAll(lines[1]).map { it.value })
+    }
 
-        for ((i, it) in digitsRegex.findAll(lines[1]).withIndex()) {
-            input[i].distance = it.value.toInt()
-        }
+    private fun countWinningRaces(time: Long, distance: Long): Int {
+        return (1..time - 1).count { distance < it * (time - it) }
     }
 
     override fun part1(): Int {
         // d < n(t - n) (where n is time holding the button)
-
-        return input
-                .map { raceInfo ->
-                    (1..raceInfo.time - 1).count { raceInfo.distance < it * (raceInfo.time - it) }
-                }
+        return times
+                .mapIndexed { i, it -> countWinningRaces(it.toLong(), distances[i].toLong()) }
                 .reduce { prev, curr -> prev * curr }
     }
 
-    override fun part2(): String {
+    override fun part2(): Int {
+        val time = times.reduce { prev, curr -> prev + curr }.let { it.toLong() }
+        val distance = distances.reduce { prev, curr -> prev + curr }.let { it.toLong() }
 
-        return "TODO"
+        return countWinningRaces(time, distance)
     }
 }
 
