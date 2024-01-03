3f352b36cd60cdcd1137be9f56fbaf84765273f0
jberg <josep.bergay@gmail.com>
2024-01-03 15:28:34 +0100

    feat: add day18 part2

--- src/2023/days/day18.kt
+++ src/2023/days/day18.kt
@@ -29,7 +29,7 @@ class Day18 : BaseDay(18) {
                 }
     }
 
-    override fun part1(): Any {
+    override fun part1(): Int {
         var minX = 0
         var minY = 0
         var maxX = 0
@@ -77,8 +77,44 @@ class Day18 : BaseDay(18) {
         return (maxX - minX + 1) * (maxY - minY + 1) - visited.size
     }
 
-    override fun part2(): Any {
-        return "TODO"
+    private fun shoelace(points: List<Point>): Long {
+        val p = buildList {
+            addAll(points)
+            add(points.first())
+        }
+
+        return p.windowed(2).sumOf { (a, b) ->
+            a.x.toLong() * b.y.toLong() - b.x.toLong() * a.y.toLong()
+        } / 2
+    }
+
+    override fun part2(): Long {
+        input =
+                input.map {
+                    val dir =
+                            when (it.color.last()) {
+                                '0' -> Direction.EAST
+                                '1' -> Direction.South
+                                '2' -> Direction.WEST
+                                '3' -> Direction.North
+                                else -> error("woot")
+                            }
+
+                    DigInstruction(dir, it.color.drop(1).dropLast(1).toInt(16), "")
+                }
+        val points = mutableListOf<Point>()
+        var perimeter = 0
+
+        var prev = Point(0, 0)
+        points += prev
+
+        for (i in input) {
+            prev = Point(i.dir.p.x * i.count, i.dir.p.y * i.count) + prev
+            perimeter += i.count
+            points += prev
+        }
+
+        return shoelace(points) + perimeter / 2L + 1L
     }
 }
 
