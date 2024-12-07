0cc7c70783be7c97140377e824953265028d8962
jberg <josep.bergay@gmail.com>
2024-12-07 18:15:41 +0100

    feat: add day1 to map

--- src/2024/days.rs
+++ src/2024/days.rs
@@ -1,11 +1,13 @@
 use std::collections::HashMap;
 
 pub use baseday::Day;
+use day1::Day1;
 
 pub mod baseday;
+pub mod day1;
 
 pub fn get_days() -> HashMap<u8, Box<dyn Day>> {
-    let all_days = HashMap::from([]);
+    let all_days = HashMap::from([(1, Box::new(Day1 {}) as Box<dyn Day>)]);
 
     all_days
 }
