7a50a518d2438e9f94a74b72e40e21cdd801b9fa
jberg <josep.bergay@gmail.com>
2024-12-08 13:24:01 +0100

    feat: add day3 part1

--- src/2024/days.rs
+++ src/2024/days.rs
@@ -3,15 +3,18 @@ use std::collections::HashMap;
 pub use baseday::Day;
 use day1::Day1;
 use day2::Day2;
+use day3::Day3;
 
 pub mod baseday;
 pub mod day1;
 pub mod day2;
+pub mod day3;
 
 pub fn get_days() -> HashMap<u8, Box<dyn Day>> {
     let all_days = HashMap::from([
         (1, Box::new(Day1::default()) as Box<dyn Day>),
         (2, Box::new(Day2::default()) as Box<dyn Day>),
+        (3, Box::new(Day3::default()) as Box<dyn Day>),
     ]);
 
     all_days
---
+++ src/2024/days/day3.rs
@@ -0,0 +1,72 @@
+use std::error::Error;
+
+use regex::Regex;
+
+use super::{baseday::DayResult, Day};
+
+#[derive(Default)]
+pub struct Day3 {}
+
+impl Day3 {
+    fn parse_input(&self, input: String) -> String {
+        input
+    }
+
+    fn part1(&self, parsed: &String) -> i32 {
+        let re = Regex::new(r"mul\(([0-9]{1,3}),([0-9]{1,3})\)").expect("Invalid regex");
+
+        let mut total = 0;
+
+        for c in re.captures_iter(parsed) {
+            let (_, digits) = c.extract();
+
+            let [a, b] = digits.map(|d| d.parse::<i32>().unwrap());
+
+            total += a * b
+        }
+
+        total
+    }
+
+    fn part2(&self, _parsed: String) -> &str {
+        "TODO"
+    }
+}
+
+impl Day for Day3 {
+    fn exec(&self, input: String) -> Result<DayResult, Box<dyn Error>> {
+        let parsed = self.parse_input(input);
+
+        let p1 = self.part1(&parsed);
+        let p2 = self.part2(parsed);
+
+        Ok(DayResult {
+            part1: p1.to_string(),
+            part2: p2.to_string(),
+        })
+    }
+}
+
+#[test]
+fn test_day3_p1() {
+    let input =
+        String::from("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))");
+
+    let day = Day3::default();
+    let parsed = day.parse_input(input);
+    let res = day.part1(&parsed);
+
+    assert_eq!(res, 161)
+}
+
+#[test]
+fn test_day3_p2() {
+    let input =
+        String::from("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))");
+
+    let day = Day3::default();
+    let parsed = day.parse_input(input);
+    let res = day.part2(parsed);
+
+    assert_eq!(res, "TODO")
+}
