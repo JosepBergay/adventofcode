abec5560d5ea64e6bdd893df43845083c0542a79
jberg <josep.bergay@gmail.com>
2024-12-13 19:57:40 +0100

    feat: add day5 part2

--- src/2024/days/day5.rs
+++ src/2024/days/day5.rs
@@ -1,7 +1,4 @@
-use std::{
-    collections::{hash_set, HashMap, HashSet},
-    error::Error,
-};
+use std::{collections::HashMap, error::Error};
 
 use super::{baseday::DayResult, Day};
 
@@ -9,7 +6,7 @@ use super::{baseday::DayResult, Day};
 pub struct Day5 {}
 
 impl Day5 {
-    fn parse_input(&self, input: String) -> (HashMap<i32, Vec<i32>>, Vec<Vec<i32>>) {
+    fn parse_input(&self, input: String) -> (HashMap<i32, Vec<i32>>, Vec<Vec<i32>>, Vec<Vec<i32>>) {
         let mut input_iter = input.split("\n\n");
 
         let rules = input_iter.next().unwrap().split("\n");
@@ -34,44 +31,82 @@ impl Day5 {
             .map(|line| line.split(',').map(|c| c.parse().unwrap()).collect())
             .collect::<Vec<Vec<i32>>>();
 
-        (deps, updates)
+        let (ordered, unordered): (Vec<Vec<i32>>, Vec<Vec<i32>>) =
+            updates.into_iter().partition(|up| {
+                let mut seen = vec![];
+
+                for page in up {
+                    if all_deps_satisfied(&page, &seen, up, &deps) {
+                        seen.push(*page);
+                    } else {
+                        break;
+                    }
+                }
+                seen.len() == up.len()
+            });
+
+        (deps, ordered, unordered)
     }
 
-    fn part1(&self, parsed: &(HashMap<i32, Vec<i32>>, Vec<Vec<i32>>)) -> i32 {
-        let (rules, updates) = parsed;
+    fn part1(&self, parsed: &(HashMap<i32, Vec<i32>>, Vec<Vec<i32>>, Vec<Vec<i32>>)) -> i32 {
+        let (_, ordered, _) = parsed;
 
-        let mut printed: Vec<&Vec<i32>> = vec![];
+        add_mid_positions(ordered)
+    }
 
-        'outer: for up in updates {
-            let mut seen = HashSet::new();
+    fn part2(&self, parsed: (HashMap<i32, Vec<i32>>, Vec<Vec<i32>>, Vec<Vec<i32>>)) -> i32 {
+        let (deps_map, _, unordered) = parsed;
 
-            for (_i, page) in up.iter().enumerate() {
-                let deps = rules.get(page);
+        let mut ordered: Vec<Vec<i32>> = vec![];
 
-                if deps.is_none_or(|ds| {
-                    ds.iter()
-                        .filter(|d| up.contains(d))
-                        .all(|d| seen.contains(d))
-                }) {
-                    seen.insert(page);
-                    continue;
-                }
+        for up in unordered {
+            let set = custom_sort(&up, &deps_map);
 
-                continue 'outer;
-            }
-
-            printed.push(up);
+            ordered.push(Vec::from_iter(set));
         }
 
-        printed.iter().fold(0, |acc, up| {
-            let mid = up[(up.len() - 1) / 2];
-            acc + mid
-        })
+        add_mid_positions(&ordered)
+    }
+}
+
+fn custom_sort(update: &Vec<i32>, deps_map: &HashMap<i32, Vec<i32>>) -> Vec<i32> {
+    let mut sorted = vec![];
+
+    let mut to_add = update.clone();
+
+    while !to_add.is_empty() {
+        let mut new_to_add = vec![];
+
+        for page in to_add {
+            if all_deps_satisfied(&page, &sorted, update, deps_map) {
+                sorted.push(page);
+            } else {
+                new_to_add.push(page);
+            }
+        }
+
+        to_add = new_to_add;
     }
 
-    fn part2(&self, _parsed: (HashMap<i32, Vec<i32>>, Vec<Vec<i32>>)) -> &str {
-        "TODO"
-    }
+    sorted
+}
+
+fn all_deps_satisfied(
+    page: &i32,
+    seen: &Vec<i32>,
+    update: &Vec<i32>,
+    deps_map: &HashMap<i32, Vec<i32>>,
+) -> bool {
+    deps_map
+        .get(page)
+        .is_none_or(|deps| deps.iter().all(|d| !update.contains(d) || seen.contains(d)))
+}
+
+fn add_mid_positions(updates: &Vec<Vec<i32>>) -> i32 {
+    updates.iter().fold(0, |acc, up| {
+        let mid = up[(up.len() - 1) / 2];
+        acc + mid
+    })
 }
 
 impl Day for Day5 {
@@ -88,9 +123,9 @@ impl Day for Day5 {
     }
 }
 
-#[test]
-fn test_day5_p1() {
-    let input = String::from(
+#[cfg(test)]
+fn get_test_input() -> String {
+    String::from(
         "47|53
 97|13
 97|61
@@ -120,7 +155,12 @@ fn test_day5_p1() {
 61,13,29
 97,13,75,29,47
 ",
-    );
+    )
+}
+
+#[test]
+fn test_day5_p1() {
+    let input = get_test_input();
 
     let day = Day5::default();
     let parsed = day.parse_input(input);
@@ -131,11 +171,11 @@ fn test_day5_p1() {
 
 #[test]
 fn test_day5_p2() {
-    let input = String::from("");
+    let input = get_test_input();
 
     let day = Day5::default();
     let parsed = day.parse_input(input);
     let res = day.part2(parsed);
 
-    assert_eq!(res, "TODO")
+    assert_eq!(res, 123)
 }
