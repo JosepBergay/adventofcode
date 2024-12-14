645b36037aae1a40b7f361546d261dcf3a798443
jberg <josep.bergay@gmail.com>
2024-12-14 01:02:27 +0100

    feat: add day7 part2

--- src/2024/days/day7.rs
+++ src/2024/days/day7.rs
@@ -26,7 +26,8 @@ impl Day7 {
         let mut sum = 0;
 
         for (res, ops) in parsed {
-            if evaluate(res, ops, 0) {
+            // Input works both for 0 and 1.
+            if evaluate_p1(res, ops, 0) {
                 sum += res;
             }
         }
@@ -34,19 +35,45 @@ impl Day7 {
         sum
     }
 
-    fn part2(&self, _parsed: Vec<(i64, Vec<i64>)>) -> i64 {
-        0
+    fn part2(&self, parsed: &Vec<(i64, Vec<i64>)>) -> i64 {
+        let mut sum = 0;
+
+        for (res, ops) in parsed {
+            // Acc start value is arbitrary and should be determined by each operation:
+            // (+ 0, * 1, || 0). Input in this case just works for 1.
+            if evaluate_p2(res, ops, 1) {
+                sum += res;
+            }
+        }
+
+        sum
     }
 }
 
-fn evaluate(res: &i64, ops: &[i64], acc: i64) -> bool {
+fn evaluate_p1(res: &i64, ops: &[i64], acc: i64) -> bool {
     if ops.is_empty() {
         return acc == *res;
     }
 
     let op = ops.first().unwrap();
 
-    evaluate(res, &ops[1..], acc + op) || evaluate(res, &ops[1..], acc * op)
+    evaluate_p1(res, &ops[1..], acc + op) || evaluate_p1(res, &ops[1..], acc * op)
+}
+
+fn evaluate_p2(res: &i64, ops: &[i64], acc: i64) -> bool {
+    if ops.is_empty() {
+        return acc == *res;
+    }
+
+    let op = ops.first().unwrap();
+
+    evaluate_p2(res, &ops[1..], acc + op)
+        || evaluate_p2(res, &ops[1..], acc * op)
+        || evaluate_p2(
+            res,
+            &ops[1..],
+            (acc.to_string() + &op.to_string()).parse::<i64>().unwrap(),
+        )
 }
 
 impl Day for Day7 {
@@ -54,7 +81,7 @@ impl Day for Day7 {
         let parsed = self.parse_input(input);
 
         let p1 = self.part1(&parsed);
-        let p2 = self.part2(parsed);
+        let p2 = self.part2(&parsed);
 
         Ok(DayResult {
             part1: p1.to_string(),
@@ -87,11 +114,22 @@ fn test_day7_p1() {
 
 #[test]
 fn test_day7_p2() {
-    let input = String::from("");
+    let input = String::from(
+        "190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
+",
+    );
 
     let day = Day7::default();
     let parsed = day.parse_input(input);
-    let res = day.part2(parsed);
+    let res = day.part2(&parsed);
 
-    assert_eq!(res, 3749)
+    assert_eq!(res, 11387)
 }
