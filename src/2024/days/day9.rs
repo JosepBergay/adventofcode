db5114fffe099710f2785086d4da28352057ad93
jberg <josep.bergay@gmail.com>
2024-12-16 21:58:54 +0100

    feat: add day9 part2

--- src/2024/days/day9.rs
+++ src/2024/days/day9.rs
@@ -1,4 +1,4 @@
-use std::error::Error;
+use std::{cmp, error::Error, usize};
 
 use super::{baseday::DayResult, Day};
 
@@ -6,12 +6,8 @@ use super::{baseday::DayResult, Day};
 pub struct Day9 {}
 
 impl Day9 {
-    fn parse_input(&self, input: String) -> String {
+    fn parse_input(&self, input: String) -> Vec<Option<usize>> {
         input
-    }
-
-    fn part1(&self, parsed: &str) -> usize {
-        let aux = parsed
             .chars()
             .filter(|c| *c != '\n')
             .enumerate()
@@ -22,21 +18,23 @@ impl Day9 {
 
                 (0..block_len).map(move |_| id)
             })
-            .collect::<Vec<_>>();
+            .collect::<Vec<_>>()
+    }
 
+    fn part1(&self, parsed: &Vec<Option<usize>>) -> usize {
         let mut acc = 0;
         let mut idx = 0;
-        let mut last_idx = aux.len();
+        let mut last_idx = parsed.len();
 
         while idx < last_idx {
-            let opt = aux[idx];
+            let opt = parsed[idx];
 
-            if let Some(c) = opt {
-                acc += idx * c
-            } else if let Some((i, o)) = aux[idx..last_idx]
+            if let Some(id) = opt {
+                acc += idx * id
+            } else if let Some((i, o)) = parsed[idx..last_idx]
                 .iter()
                 .enumerate()
-                .rfind(|(_, &o)| o.is_some())
+                .rfind(|(_, o)| o.is_some())
             {
                 acc += idx * o.unwrap();
                 last_idx = idx + i;
@@ -48,9 +46,71 @@ impl Day9 {
         acc
     }
 
-    fn part2(&self, _parsed: String) -> usize {
-        0
+    fn part2(&self, mut parsed: Vec<Option<usize>>) -> usize {
+        let mut last_idx = parsed.len() - 1;
+
+        let mut min_space_available = usize::MAX;
+
+        loop {
+            let last = parsed[..=last_idx]
+                .iter()
+                .enumerate()
+                .rfind(|(_, o)| o.is_some());
+
+            if let Some((i, Some(id))) = last {
+                let opt = parsed[..i]
+                    .iter()
+                    .rposition(|o| o.is_none_or(|id2| id2 != *id));
+
+                if opt.is_none() {
+                    break;
+                }
+
+                let j = opt.unwrap();
+
+                let file_size = i - j;
+                last_idx = j;
+
+                if file_size >= min_space_available {
+                    continue;
+                }
+
+                if let Some(insert_at) = find_empty_space(file_size, &parsed[..=j]) {
+                    for k in 0..file_size {
+                        parsed.swap(insert_at + k, last_idx + 1 + k);
+                    }
+                } else {
+                    min_space_available = cmp::min(file_size, min_space_available);
+                }
+            }
+        }
+
+        parsed
+            .iter()
+            .enumerate()
+            .fold(0, |acc, (i, opt)| match opt {
+                Some(id) => acc + i * id,
+                None => acc,
+            })
     }
+}
+
+fn find_empty_space(size: usize, parsed: &[Option<usize>]) -> Option<usize> {
+    let mut empty_space = 0;
+
+    for (i, opt) in parsed.iter().enumerate() {
+        if opt.is_some() && empty_space != 0 {
+            empty_space = 0;
+        } else if opt.is_none() {
+            empty_space += 1;
+
+            if empty_space == size {
+                return Some(1 + i - size);
+            }
+        }
+    }
+
+    None
 }
 
 impl Day for Day9 {
@@ -80,7 +140,7 @@ fn test_day9_p1() {
 
 #[test]
 fn test_day9_p2() {
-    let input = String::from("");
+    let input = String::from("2333133121414131402");
 
     let day = Day9::default();
     let parsed = day.parse_input(input);
