a3c7b969402a7c210e3f8c8513d3ec58fd8b2172
jberg <josep.bergay@gmail.com>
2025-01-02 19:32:22 +0100

    feat: add day20 part2

--- src/2024/days/day20.rs
+++ src/2024/days/day20.rs
@@ -10,7 +10,7 @@ use super::{baseday::DayResult, point2d::Point2D, Day};
 #[derive(Default)]
 pub struct Day20 {}
 
-type Input = (Point2D, Point2D, Map2D<char>);
+type Input = (Vec<Point2D>, Map2D<char>);
 
 impl Day20 {
     fn parse_input(&self, input: String) -> Input {
@@ -27,23 +27,18 @@ impl Day20 {
             }
         }
 
-        (start, end, map)
-    }
-
-    fn part1(&self, parsed: &Input, savings: usize) -> usize {
-        let (start, end, map) = parsed;
         let mut path = vec![];
         let mut seen = HashSet::new();
 
         let mut q = VecDeque::new();
 
-        q.push_back(*start);
+        q.push_back(start);
 
         while let Some(curr) = q.pop_front() {
             path.push(curr);
             seen.insert(curr);
 
-            if curr == *end {
+            if curr == end {
                 break;
             }
 
@@ -54,39 +49,77 @@ impl Day20 {
             }
         }
 
-        let dists = vec![
-            Point2D::new(2, 0),
-            Point2D::new(1, 1),
-            Point2D::new(0, 2),
-            Point2D::new(-1, 1),
-            Point2D::new(-2, 0),
-            Point2D::new(-1, -1),
-            Point2D::new(0, -2),
-            Point2D::new(1, -1),
-        ];
+        (path, map)
+    }
+
+    fn part1(&self, parsed: &Input, savings: usize) -> usize {
+        let (path, map) = parsed;
+
+        let dists = get_points_at(2);
 
         path[..path.len() - 2]
             .iter()
             .enumerate()
             .flat_map(|(i, p)| {
-                let path_left = &path[i + 2..];
-
-                let cheats = dists.iter().map(|d| *p + *d).filter(|&next| {
-                    map.get(next).is_some_and(|c| *c != '#')
-                        && path_left
-                            .iter()
-                            .position(|it| *it == next)
-                            .is_some_and(|j| j >= savings)
-                });
-
-                cheats
+                dists
+                    .iter()
+                    .map(|d| *p + *d)
+                    .filter(move |&next| {
+                        map.get(next).is_some_and(|c| *c != '#')
+                            && path[i + 2..]
+                                .iter()
+                                .position(|it| *it == next)
+                                .is_some_and(|j| j >= savings)
+                    })
+                    .collect::<Vec<Point2D>>()
             })
             .count()
     }
 
-    fn part2(&self, _parsed: Input) -> &str {
-        "TODO"
+    fn part2(&self, parsed: Input, savings: usize) -> usize {
+        let (path, map) = parsed;
+
+        let cheat_dists: Vec<_> = (2..=20).flat_map(|n| get_points_at(n)).collect();
+
+        let mut count = 0;
+
+        for (i, p) in path.iter().enumerate() {
+            let cheat_ends = cheat_dists.iter().filter(|&d| {
+                let dist = (d.x.abs() + d.y.abs()) as usize;
+
+                if dist > path.len() - i {
+                    return false;
+                }
+
+                let next = *p + *d;
+                map.get(next).is_some_and(|c| *c != '#')
+                    && path[i + dist..]
+                        .iter()
+                        .position(|it| *it == next)
+                        .is_some_and(|j| j >= savings)
+            });
+
+            count += cheat_ends.count();
+        }
+
+        count
     }
+}
+
+fn get_points_at(manhattan_dist: i32) -> Vec<Point2D> {
+    let manhattan_dist = manhattan_dist.abs();
+
+    let mut out = vec![];
+
+    for x in -manhattan_dist..=manhattan_dist {
+        for y in -manhattan_dist..=manhattan_dist {
+            if x.abs() + y.abs() == manhattan_dist {
+                out.push(Point2D::new(x, y));
+            }
+        }
+    }
+
+    out
 }
 
 impl Day for Day20 {
@@ -94,7 +127,7 @@ impl Day for Day20 {
         let parsed = self.parse_input(input);
 
         let p1 = self.part1(&parsed, 100);
-        let p2 = self.part2(parsed);
+        let p2 = self.part2(parsed, 100);
 
         Ok(DayResult {
             part1: p1.to_string(),
@@ -103,9 +136,9 @@ impl Day for Day20 {
     }
 }
 
-#[test]
-fn test_day20_p1() {
-    let input = String::from(
+#[cfg(test)]
+fn get_test_input() -> String {
+    String::from(
         "###############
 #...#...#.....#
 #.#.#.#.#.###.#
@@ -122,22 +155,27 @@ fn test_day20_p1() {
 #...#...#...###
 ###############
 ",
-    );
+    )
+}
+
+#[test]
+fn test_day20_p1() {
+    let input = get_test_input();
 
     let day = Day20::default();
     let parsed = day.parse_input(input);
-
     let res = day.part1(&parsed, 2);
+
     assert_eq!(res, 44);
 }
 
 #[test]
 fn test_day20_p2() {
-    let input = String::from("");
+    let input = get_test_input();
 
     let day = Day20::default();
     let parsed = day.parse_input(input);
-    let res = day.part2(parsed);
+    let res = day.part2(parsed, 50);
 
-    assert_eq!(res, "TODO")
+    assert_eq!(res, 285);
 }
