ce8895f0a68a650a791ee4bb5217f71f75c366d5
jberg <josep.bergay@gmail.com>
2025-12-13 16:38:37 +0100

    feat: add day3 part2

--- src/2025/Days/Day3.cs
+++ src/2025/Days/Day3.cs
@@ -2,6 +2,8 @@ using Microsoft.VisualBasic;
 
 public class Day3 : BaseDay<string>
 {
+    private readonly int zero = '0';
+
     public override string Parse(string input)
     {
         return input;
@@ -42,6 +44,70 @@ public class Day3 : BaseDay<string>
 
     public override string Part2(string parsed)
     {
-        return "";
+        long total = 0;
+
+        var digits = new int[12];
+        long currBest = 0;
+        foreach (var c in parsed)
+        {
+            if (c == '\n')
+            {
+                total += currBest;
+                digits = new int[12];
+                currBest = 0;
+                continue;
+            }
+
+            var replaceIdx = digits.Length - 1;
+            for (int i = 0; i < digits.Length - 1; i++)
+            {
+                if (digits[i] < digits[i + 1])
+                {
+                    replaceIdx = i;
+                    break;
+                }
+            }
+
+
+            long candidate = 0;
+            for (int i = 0; i < digits.Length; i++)
+            {
+                if (i == replaceIdx) continue;
+
+                var d = digits[i];
+                if (i < replaceIdx) candidate += (long)(d * Math.Pow(10, 11 - i));
+                else
+                    // i > minIdx
+                    candidate += (long)(d * Math.Pow(10, 12 - i)); // Shift digit left
+            }
+
+            var n = c - zero;
+            candidate += n; // Always add current char at the end
+
+            if (candidate > currBest)
+            {
+                for (int i = 0; i < digits.Length; i++)
+                {
+                    if (i >= replaceIdx)
+                    {
+                        if (i == digits.Length - 1)
+                        {
+                            digits[i] = n;
+                        }
+                        else
+                        {
+                            digits[i] = digits[i + 1];
+                        }
+                    }
+                }
+
+                currBest = candidate;
+            }
+        }
+
+        // In case input does not end with '\n'.
+        total += currBest;
+
+        return total.ToString();
     }
 }
--- src/2025/Days/Program.cs
+++ src/2025/Days/Program.cs
@@ -2,6 +2,7 @@
 var allDays = new Dictionary<int, IDay>{
     { 1, new Day1() },
     { 2, new Day2() },
+    { 3, new Day3() },
 };
 
 DayRunner runner = new();
