e7e9b326fd5cc4d82e9a60eda0c4e8e1363a8319
jberg <josep.bergay@gmail.com>
2025-12-27 20:53:06 +0100

    refactor: move direction to point2d file

--- src/2025/Days/Point2D.cs
+++ src/2025/Days/Point2D.cs
@@ -7,3 +7,15 @@ public record Point2D(int x, int y) : IAdditionOperators<Point2D, Point2D, Point
         return new Point2D(left.x + right.x, left.y + right.y);
     }
 }
+
+public static class Direction
+{
+    public static readonly Point2D N = new(0, 1);
+    public static readonly Point2D NE = new(1, 1);
+    public static readonly Point2D E = new(1, 0);
+    public static readonly Point2D SE = new(1, -1);
+    public static readonly Point2D S = new(0, -1);
+    public static readonly Point2D SW = new(-1, -1);
+    public static readonly Point2D W = new(-1, 0);
+    public static readonly Point2D NW = new(-1, 1);
+}
