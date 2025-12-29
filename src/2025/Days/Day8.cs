3c0679d2e7532c04f7231a79803e6e8f2e3b338b
jberg <josep.bergay@gmail.com>
2025-12-29 21:47:41 +0100

    feat: add day8 part2

--- src/2025/Days/Day8.cs
+++ src/2025/Days/Day8.cs
@@ -1,8 +1,10 @@
 using System.Numerics;
 
-public class Day8 : BaseDay<(Point3D, Point3D, double)[]>
+using T = ((Point3D P1, Point3D P2, double Distance)[] Connections, int Count);
+
+public class Day8 : BaseDay<T>
 {
-    public override (Point3D, Point3D, double)[] Parse(string input)
+    public override T Parse(string input)
     {
         var list = new List<Point3D>();
 
@@ -17,22 +19,21 @@ public class Day8 : BaseDay<(Point3D, Point3D, double)[]>
             list.Add(Point3D.From(line.Split(",").Select(int.Parse)));
         }
 
-        return list
+        return ([.. list
             .SkipLast(1)
             .SelectMany((p1, y) =>
                 list
                     .Skip(y + 1)
                     .Select(p2 => (p1, p2, distance: p1.Distance(p2))))
-            .OrderBy(it => it.distance)
-            .ToArray();
+            .OrderBy(it => it.distance)], list.Count);
     }
 
-    public override string Part1((Point3D, Point3D, double)[] parsed)
+    public override string Part1(T parsed)
     {
-        var maxCount = parsed.Length > 20 ? 1000 : 10;
+        var maxCount = parsed.Count > 20 ? 1000 : 10;
         var adjDict = new Dictionary<Point3D, HashSet<Point3D>>();
 
-        foreach (var (p1, p2, _) in parsed.Take(maxCount))
+        foreach (var (p1, p2, _) in parsed.Connections.Take(maxCount))
         {
             if (!adjDict.TryGetValue(p1, out HashSet<Point3D>? neighbours1))
             {
@@ -80,9 +81,43 @@ public class Day8 : BaseDay<(Point3D, Point3D, double)[]>
             .ToString();
     }
 
-    public override string Part2((Point3D, Point3D, double)[] parsed)
+    public override string Part2(T parsed)
     {
-        return "";
+        var circuits = new List<HashSet<Point3D>>();
+        var i = 0;
+
+        while (circuits.Count != 1 || circuits.First().Count != parsed.Count)
+        {
+            var (P1, P2, Distance) = parsed.Connections[i++];
+
+            var c1 = circuits.Find(c => c.Contains(P1));
+            var c2 = circuits.Find(c => c.Contains(P2));
+
+            if (c1 is null && c2 is null)
+            {
+                // New circuit
+                circuits.Add([P1, P2]);
+            }
+            else if (c1 == c2)
+            {
+                // Already added in same circuit
+            }
+            else if (c1 is not null && c2 is not null)
+            {
+                // Merge circuits
+                c1.UnionWith(c2);
+                circuits.Remove(c2);
+            }
+            else
+            {
+                c1?.UnionWith([P1, P2]);
+                c2?.UnionWith([P1, P2]);
+            }
+        }
+
+        var connection = parsed.Connections[i - 1];
+
+        return ((long)connection.P1.X * connection.P2.X).ToString();
     }
 }
 
@@ -103,5 +138,4 @@ public record Point3D(int X, int Y, int Z) : ISubtractionOperators<Point3D, Poin
     public double Length() => Math.Sqrt((long)X * (long)X + (long)Y * (long)Y + (long)Z * (long)Z);
 
     public double Distance(Point3D other) => (this - other).Length();
-
 }
